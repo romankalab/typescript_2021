b01d85a4a7186ccc7dbf48219f6b464c00cdcffd
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-09-30 10:23:43 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let result;
+let a = 2;
+let b = 3;
+let c = 4;
+if (a+b>c)
+{
+    if (b+c>a)
+    {
+        if (a+c>b)
+        {
+            result = true;
+        }
+    }
+}
+
+else
+{
+result = false;
+}
+console.log(result);
