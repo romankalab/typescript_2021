321a2d714bd1b5362b63bacd1a7edf0d6da91a2f
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-09-30 09:53:53 +0200

    comleted

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 let a =2;
 let b = 3;
 let c = 4;
-console.log(a+b+c);
+let equality = 0;
+if (a == b)
+    equality++;
+if (b == c)
+    equality++;
+if (a != b && a == c)
+    equality++;
+if (equality != 0)
+    equality++;
+console.log(equality);
