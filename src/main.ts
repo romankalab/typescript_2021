a3368c803d9cf2d9a0a0f610ca6b1a4043af3ea9
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-09-30 10:33:10 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let totalSlices = 11;
+let people = 5;
+let slicesEach = 2;
+let equalSlices;
+
+if (totalSlices >= (people*slicesEach))
+    equalSlices = true;
+else
+    equalSlices = false;
+
+console.log(equalSlices);
