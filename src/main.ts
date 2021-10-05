57bedd300ff70b90fab25eab0f941f96e92148d6
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-10-05 12:02:33 +0200

    nedokoncene

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,16 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let array = [1,2,3,4,5,6,7];
+let even_number_sum;
+let odd_number_sum;
+array.forEach(number => {if(number%2==0)
+            even_number_sum+=number;});
+array.forEach(number => {if(number%2==1)
+            odd_number_sum+=number;});
+
+if (even_number_sum > odd_number_sum)
+{
+    console.log(even_number_sum-odd_number_sum);
+}
+else
+{
+    console.log(odd_number_sum-even_number_sum);
+}
