bad5011542581835c411116e14eb12460a56ef89
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-10-05 11:19:52 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,51 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let r = "rock";
+let p = "paper";
+let s = "scissors";
+let p1 = r;
+let p2 = p;
+
+if (p1 == r)
+{
+    if (p2 == r)
+    {
+        console.log("TIE.");
+    }
+    if (p2 == p)
+    {
+        console.log("Player2 wins.");
+    }
+    if (p2 == s)
+    {
+        console.log("Player1 wins.");
+    }
+}
+if (p1 == p)
+{
+    if (p2 == r)
+    {
+        console.log("Player1 wins.");
+    }
+    if (p2 == p)
+    {
+        console.log("TIE.");
+    }
+    if (p2 == s)
+    {
+        console.log("Player2 wins.");
+    }
+}
+if (p1 == s)
+{
+    if (p2 == r)
+    {
+        console.log("Player2 wins.");
+    }
+    if (p2 == p)
+    {
+        console.log("Player1 wins.");
+    }
+    if (p2 == s)
+    {
+        console.log("TIE.");
+    }
+}
