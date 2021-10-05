852878540379d02c1ea93d8972eff0d3285a837a
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-10-05 11:43:02 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let array = [4,5,1,6,8,3];
+array.sort((a, b) => a - b);
+let smallest_number = array[0];
+let biggest_number = array[array.length-1];
+console.log("Smallest number is " + smallest_number 
+            + ", biggest number is " + biggest_number 
+            + ". The difference is " + (biggest_number-smallest_number) 
+            + ".");
