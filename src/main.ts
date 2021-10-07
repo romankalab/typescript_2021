37179e57a50d1eb76a1c48423af3a1d450031d63
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-10-07 09:48:38 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-let array = [1,2,3,4,5,6,7];
-let even_number_sum;
-let odd_number_sum;
+let array = [1,7,2,4,8,5,101,104,12,15];
+let even_number_sum = 0;
+let odd_number_sum = 0;
 array.forEach(number => {if(number%2==0)
             even_number_sum+=number;});
 array.forEach(number => {if(number%2==1)
