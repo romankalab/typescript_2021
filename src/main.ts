d392c1cd112b42888599dd1aab29ffcc47330247
PePeKuBk0 <jakub.venetianer@gmail.com>
2021-10-07 10:13:23 +0200

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-const message: string = 'Hello Appslab 2020!';
-console.log(message);
+let white_rook = "B6";
+let black_rook = "A8";
+if (white_rook.charAt(0) == black_rook.charAt(0) ||
+    white_rook.charAt(1) == black_rook.charAt(1))
+{
+    console.log(true);
+}
+else
+{
+    console.log(false);
+}
